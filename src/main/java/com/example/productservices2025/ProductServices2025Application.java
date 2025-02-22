1b8aec335e657696cc9cc4c6adb336a1f4ec04b1
Vandana Purushothaman <vandanapurushothaman333@gmail.com>
2025-02-22 22:06:49 +0530

    FistCommit

---
+++ src/main/java/com/example/productservices2025/ProductServices2025Application.java
@@ -0,0 +1,14 @@
+package com.example.productservices2025;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServices2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServices2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/productservices2025/ProductServices2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productservices2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServices2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
